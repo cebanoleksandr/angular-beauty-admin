426e4d51cd06cc560d0b67284a5c99b6dcd000c6
Oleksandr Cheban <cebanoleksandr@gmail.com>
2025-11-28 16:56:53 +0200

    Fix modules

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
-import { AuthLayoutModule } from './shared/components/layouts/auth-layout/auth-layout-module';
-import { MainLayoutModule } from './shared/components/layouts/main-layout/main-layout-module';
+import { FeaturesModule } from './features/features-module';
+import { SharedModule } from './shared/shared-module';
 
 @NgModule({
   declarations: [
@@ -13,8 +13,8 @@ import { MainLayoutModule } from './shared/components/layouts/main-layout/main-l
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AuthLayoutModule,
-    MainLayoutModule,
+    FeaturesModule,
+    SharedModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
---
+++ src/app/shared/shared-module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgIconsModule } from '@ng-icons/core';
+import { 
+  heroBars3Solid, 
+  heroBeakerSolid, 
+  heroCalendarSolid, 
+  heroCog6ToothSolid, 
+  heroHomeSolid, 
+  heroSparklesSolid, 
+  heroUserGroupSolid, 
+  heroUsersSolid 
+} from '@ng-icons/heroicons/solid'; 
+import { heroArrowRightStartOnRectangle, heroMagnifyingGlass } from '@ng-icons/heroicons/outline';
+import { RouterModule } from '@angular/router';
+import { AuthLayout as AuthLayoutComponent } from './components/layouts/auth-layout/auth-layout';
+import { MainLayout as MainLayoutComponent } from './components/layouts/main-layout/main-layout';
+import { HeaderComponent } from './components/layouts/header/header';
+import { Sidebar as SidebarComponent } from './components/layouts/sidebar/sidebar';
+
+@NgModule({
+  declarations: [
+    MainLayoutComponent,
+    SidebarComponent,
+    HeaderComponent,
+    AuthLayoutComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    NgIconsModule.withIcons({ 
+      heroBars3Solid,
+      heroMagnifyingGlass,
+      heroArrowRightStartOnRectangle,
+      heroHomeSolid,
+      heroSparklesSolid,
+      heroCalendarSolid,
+      heroCog6ToothSolid,
+      heroUserGroupSolid,
+      heroUsersSolid,
+      heroBeakerSolid,
+    })
+  ],
+  exports: [
+    MainLayoutComponent,
+    SidebarComponent,
+    HeaderComponent,
+    AuthLayoutComponent,
+  ]
+})
+export class SharedModule { }
